0fb98f64f01316b759c9fd45abdf0352c52407bd
Okoh Ugochukwu <edokoh9@Okohs-MacBook-Air.local>
2020-12-25 10:08:00 -0600

    server commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const next = require('next')
-
+const bodyParser = require('body-parser');
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -8,7 +8,8 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
     const server = express()
-
+    server.use(bodyParser.urlencoded({ extended: true }));
+    server.use(bodyParser.json());
     // server.all('*', (req, res) => {
     //     return handle(req, res)
     // })
