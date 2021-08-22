f360aead902140c81bbc6d6053a7126a0602da05
Ugo Okoh <ugo@Ugos-MacBook-Pro.local>
2021-08-21 19:40:58 -0700

    Made changes to forever and implemented new way of starting server

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e13ed633e50a9ac1988f";
+/******/ 	var hotCurrentHash = "eaffbd63115a6dd112f7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
